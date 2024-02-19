e7ef92e31052a093988c7d4bdb74ad33016bdb4e
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-19 10:55:00 +0100

    restuct code and assets

--- src/consts.py
+++ src/consts.py
@@ -8,6 +8,15 @@ import pygame
 
 # os.path.join(os.path.dirname(__file__), "assets")
 ASSETS_DIR = "./assets/"
+IMG_DIR = os.path.join(ASSETS_DIR, "imgs")
+SOUND_DIR = os.path.join(ASSETS_DIR, "sounds")
+
+
+grenade = os.path.join(SOUND_DIR, "Grenade+1.mp3")
+gun = os.path.join(SOUND_DIR, "Gun+Silencer.mp3")
+spaceship_red = os.path.join(IMG_DIR, "spaceship_red.png")
+spaceship_yellow = os.path.join(IMG_DIR, "spaceship_yellow.png")
+space = os.path.join(IMG_DIR, "space.png")
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -21,8 +30,8 @@ BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
 
 
 # TODO: Debug this
-BULLET_HIT_SOUND = pygame.mixer.Sound("./assets/Grenade+1.mp3")
-BULLET_FIRE_SOUND = pygame.mixer.Sound("./assets/Gun+Silencer.mp3")
+BULLET_HIT_SOUND = pygame.mixer.Sound(grenade)
+BULLET_FIRE_SOUND = pygame.mixer.Sound(gun)
 
 
 HEALTH_FONT = pygame.font.SysFont("comicsans", 40)
@@ -37,20 +46,16 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
 
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(
-    os.path.join(ASSETS_DIR, "spaceship_yellow.png")
-)
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join(spaceship_yellow))
 YELLOW_SPACESHIP = pygame.transform.rotate(
     pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),
     90,
 )
 
-RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join(ASSETS_DIR, "spaceship_red.png"))
+RED_SPACESHIP_IMAGE = pygame.image.load(spaceship_red)
 RED_SPACESHIP = pygame.transform.rotate(
     pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),
     270,
 )
 
-SPACE = pygame.transform.scale(
-    pygame.image.load(os.path.join(ASSETS_DIR, "space.png")), (WIDTH, HEIGHT)
-)
+SPACE = pygame.transform.scale(pygame.image.load(space), (WIDTH, HEIGHT))
--- src/events_loop.py
+++ src/events_loop.py
@@ -1,3 +1,88 @@
 """
 This module contains the main event loop for the game.
 """
+
+from .consts import *
+from .functs import *
+
+
+def loop_event(
+    yellow_bullets,
+    red_bullets,
+    yellow,
+    red,
+    red_health,
+    yellow_health,
+):
+    """main manager for event loop in the game."""
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                bullet = pygame.Rect(
+                    yellow.x + yellow.width,
+                    yellow.y + yellow.height // 2 - 2,
+                    10,
+                    5,
+                )
+                yellow_bullets.append(bullet)
+                BULLET_FIRE_SOUND.play()
+
+            if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                bullet = pygame.Rect(red.x, red.y + red.height // 2 - 2, 10, 5)
+                red_bullets.append(bullet)
+                BULLET_FIRE_SOUND.play()
+
+        if event.type == RED_HIT:
+            red_health -= 1
+            BULLET_HIT_SOUND.play()
+
+        if event.type == YELLOW_HIT:
+            yellow_health -= 1
+            BULLET_HIT_SOUND.play()
+
+    return red_health, yellow_health
+
+
+def main_loop(
+    yellow_bullets, red_bullets, yellow, red, red_health, yellow_health, clock
+):
+    clock.tick(FPS)
+
+    red_health, yellow_health = loop_event(
+        yellow_bullets,
+        red_bullets,
+        yellow,
+        red,
+        red_health,
+        yellow_health,
+    )
+
+    winner_text = ""
+    if red_health <= 0:
+        winner_text = "Yellow Wins!"
+
+    if yellow_health <= 0:
+        winner_text = "Red Wins!"
+
+    if winner_text != "":
+        draw_winner(winner_text)
+        # break
+
+        # NOTE Maybe problematic due to expected break loop
+
+        return 0
+
+    keys_pressed = pygame.key.get_pressed()
+
+    yellow_handle_movement(keys_pressed, yellow)
+    red_handle_movement(keys_pressed, red)
+
+    handle_bullets(yellow_bullets, red_bullets, yellow, red)
+
+    draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
+
+    return red_health, yellow_health
